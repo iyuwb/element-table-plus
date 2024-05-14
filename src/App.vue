8b929df3855926d30f8b4da0f9c5297428a0b231
YEVIN <iyuwb@outlook.com>
2024-05-14 21:59:58 +0800

    init:项目初始化

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <elementTablePlus :option="option" />
+  </div>
+</template>
+
+<script>
+import elementTablePlus from "./package/element-table-plus.vue";
+export default {
+  name: "App",
+  components: {
+    elementTablePlus,
+  },
+  data() {
+    return {
+      option: {},
+    };
+  },
+  created() {
+    this.option = require("./data.json");
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin: 100px;
+  background-color: pink;
+  border: 1px solid #eee;
+}
+</style>
---
+++ src/package/element-table-plus.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="elementTablePlus">
+    <el-table :border="true" :data="option.tableData" style="width: 100%">
+      <el-table-column
+        v-for="(item,index) in option.headerData"
+        :key="item.value"
+        :align="'center'"
+        prop="date"
+        label="日期"
+        width="180"
+      >
+        <div slot="header">
+          <span> {{ item.name }} {{ index }} {{ option.tableData[index].name }}</span>
+          <el-tooltip
+            class="item"
+            effect="dark"
+            content="Top Center 提示文字"
+            placement="top"
+          >
+            <el-button> {{ item.name }}</el-button>
+          </el-tooltip>
+        </div>
+        <template slot-scope="scope">
+          <span>{{ scope.row[item.value] }}</span>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+  
+  <script>
+export default {
+  name: "elementTablePlus",
+  props: ["option"],
+  data() {
+    return {};
+  },
+};
+</script>
+  
+<style>
+#elementTablePlus {
+}
+</style>
