bc6af53ff52161a18ddb0d033ee519ebb6b8b1fc
yuwb <yuwb0521@outlook.com>
2024-05-15 21:38:22 +0800

    feat:多级表头

---
+++ src/package/el-table-column-plus.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div v-for="(item, index) in option" :key="item.value">
+      <!-- 多级表头 -->
+      <el-table-column
+        v-if="item.children && item.children.length"
+        :align="'center'"
+        prop="date"
+        label="日期"
+        width="180"
+      >
+        <div slot="header">
+          <span> {{ item.name }} {{ index }}</span>
+          <el-tooltip
+            class="item"
+            effect="dark"
+            content="Top Center 提示文字"
+            placement="top"
+          >
+            <el-button> {{ item.name }}</el-button>
+          </el-tooltip>
+        </div>
+        <elTableColumnPlus :option="item.children"> </elTableColumnPlus>
+      </el-table-column>
+      <el-table-column
+        v-else
+        :align="'center'"
+        prop="date"
+        label="日期"
+        width="180"
+      >
+        <div slot="header">
+          <span> {{ item.name }} {{ index }}</span>
+          <el-tooltip
+            class="item"
+            effect="dark"
+            content="Top Center 提示文字"
+            placement="top"
+          >
+            <el-button> {{ item.name }}</el-button>
+          </el-tooltip>
+        </div>
+        <template slot-scope="scope">
+          <span>{{ scope.row[item.value] }}</span>
+        </template>
+      </el-table-column>
+    </div>
+  </div>
+</template>
+
+<script>
+// import elTableColumnPlus from "./el-table-column-plus";
+export default {
+  name: "elTableColumnPlus",
+  props: ["option"],
+  components: {
+    // elTableColumnPlus,
+  },
+};
+</script>
+
+<style>
+</style>
--- src/package/element-table-plus.vue
+++ src/package/element-table-plus.vue
@@ -1,36 +1,24 @@
 <template>
   <div id="elementTablePlus">
-    <el-table :border="true" :data="option.tableData" style="width: 100%">
-      <el-table-column
-        v-for="(item,index) in option.headerData"
-        :key="item.value"
-        :align="'center'"
-        prop="date"
-        label="日期"
-        width="180"
-      >
-        <div slot="header">
-          <span> {{ item.name }} {{ index }} {{ option.tableData[index].name }}</span>
-          <el-tooltip
-            class="item"
-            effect="dark"
-            content="Top Center 提示文字"
-            placement="top"
-          >
-            <el-button> {{ item.name }}</el-button>
-          </el-tooltip>
-        </div>
-        <template slot-scope="scope">
-          <span>{{ scope.row[item.value] }}</span>
-        </template>
-      </el-table-column>
+    <el-table
+      :header-cell-style="{ background: '#ccc', color: '#333' }"
+      :row-style="{ background: '#ccc', color: '#666' }"
+      :border="true"
+      :data="option.tableData"
+      style="width: 100%"
+    >
+      <elTableColumnPlus :option="option.headerData"></elTableColumnPlus>
     </el-table>
   </div>
 </template>
   
   <script>
+import elTableColumnPlus from "./el-table-column-plus";
 export default {
   name: "elementTablePlus",
+  components: {
+    elTableColumnPlus,
+  },
   props: ["option"],
   data() {
     return {};
