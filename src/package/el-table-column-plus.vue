a43d60359f93b5ee896c890ad1e5a68dc73e2409
Yewen <iyuwb@outlook.com>
2024-05-16 21:38:38 +0800

    feat:多级表头

--- src/package/el-table-column-plus.vue
+++ src/package/el-table-column-plus.vue
@@ -1,5 +1,9 @@
 <template>
   <el-table-column :prop="item.prop" :label="item.label">
+    <!-- 自定义表头 -->
+    <div slot="header">
+      {{ item.label }}
+    </div>
     <template v-if="item.children && item.children.length">
       <elTableColumnPlus v-for="it in item.children" :key="it.value" :item="it"></elTableColumnPlus>
     </template>
--- src/package/element-table-plus.vue
+++ src/package/element-table-plus.vue
@@ -14,7 +14,6 @@ export default {
   components: {
     elTableColumnPlus,
   },
-
 };
 </script>
 
