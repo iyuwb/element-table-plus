df3620752cab8ba01dcbfcf9a659ed5e615db6a3
yuwb <yuwb0521@outlook.com>
2024-05-16 22:52:30 +0800

    feat:自定义表头和内容

--- src/package/el-table-column-plus.vue
+++ src/package/el-table-column-plus.vue
@@ -4,6 +4,11 @@
     <div slot="header">
       {{ item.label }}
     </div>
+    <!-- 自定义内容 -->
+    <template slot-scope="scope">
+    {{ scope.row[item.prop] }}
+    </template>
+    <!-- 嵌套 多级表头 -->
     <template v-if="item.children && item.children.length">
       <elTableColumnPlus v-for="it in item.children" :key="it.value" :item="it"></elTableColumnPlus>
     </template>
