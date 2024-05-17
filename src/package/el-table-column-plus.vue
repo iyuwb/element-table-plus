359f6dcaf1dcb64e5f2cd1955358de7435ee5d0f
yuwb <yuwb0521@outlook.com>
2024-05-17 12:56:58 +0800

    feat:自定义表头和内容

--- src/package/el-table-column-plus.vue
+++ src/package/el-table-column-plus.vue
@@ -1,16 +1,25 @@
 <template>
-  <el-table-column :prop="item.prop" :label="item.label">
+  <el-table-column
+    :prop="item.prop"
+    :label="item.label"
+    :align="item.align || 'center'"
+    :show-overflow-tooltip = "item.showOverflowTooltip"
+  >
     <!-- 自定义表头 -->
     <div slot="header">
       {{ item.label }}
     </div>
     <!-- 自定义内容 -->
     <template slot-scope="scope">
-    {{ scope.row[item.prop] }}
+      {{ scope.row[item.prop]}}
     </template>
     <!-- 嵌套 多级表头 -->
     <template v-if="item.children && item.children.length">
-      <elTableColumnPlus v-for="it in item.children" :key="it.value" :item="it"></elTableColumnPlus>
+      <elTableColumnPlus
+        v-for="it in item.children"
+        :key="it.value"
+        :item="it"
+      ></elTableColumnPlus>
     </template>
   </el-table-column>
 </template>
--- src/package/element-table-plus.vue
+++ src/package/element-table-plus.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="elementTablePlus">
-    <el-table :border="true" :data="option.tableData" style="width: 100%">
-      <elTableColumnPlus v-for="item in option.headerData" :key="item.value" :item="item"></elTableColumnPlus>
+    <el-table
+      :border="true"
+      :data="option.tableData"
+      style="width: 100%"
+      :header-cell-style="{ background: '#ccc', color: '#333' }"
+      :row-style="{ background: '#ccc', color: '#666' }"
+    >
+      <elTableColumnPlus
+        v-for="item in option.headerData"
+        :key="item.value"
+        :item="item"
+      ></elTableColumnPlus>
     </el-table>
   </div>
 </template>
@@ -9,7 +19,7 @@
 <script>
 import elTableColumnPlus from "./el-table-column-plus.vue";
 export default {
-  props: ['option'],
+  props: ["option"],
   name: "elementTablePlus",
   components: {
     elTableColumnPlus,
@@ -18,5 +28,6 @@ export default {
 </script>
 
 <style>
-#elementTablePlus {}
+#elementTablePlus {
+}
 </style>
